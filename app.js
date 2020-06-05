3e5b35828a442328257510096a86ac3bab33248a
Ankur Anant <ankuranant99@gmail.com>
2020-06-05 18:25:54 +0530

    endpoint route:

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const handlebars = require('express-handlebars');
 
 const app = express();
 
@@ -11,6 +12,12 @@ const v1Routes = require("./v1/index");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
+}));
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+  defaultLayout: 'index',
+  layoutsDir: __dirname + '/views',
 }));
 
 app.use("/v1", v1Routes);
