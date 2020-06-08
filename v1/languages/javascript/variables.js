17b4345afc67717b65d4db70675bce358bfcf364
tiafreeman <tiajane.freeman@gmail.com>
2020-06-08 12:24:38 +1000

    added description of javascript feature: variables

---
+++ v1/languages/javascript/variables.js
@@ -0,0 +1,8 @@
+const Variables = {
+  name: "variables",
+  description:
+    "Javascript variables are like containers that we can use to store information",
+  example: "var x = 5; \n var y = 4 \n var z = x + y // z is equal to 9",
+};
+
+module.exports = Variables;
