78690125c31e612bc75dd3faa4feac00a63914b3
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2020-06-11 12:29:18 +0530

    Default layout updated
    defaultlayout now uses layout instead if index
    and code style fixed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
 
 const app = express();
 
@@ -10,18 +10,23 @@ const PORT = process.env.PORT || 3007;
 const v1Routes = require("./v1/index");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
 
-app.set('view engine', 'handlebars');
-app.engine('handlebars', handlebars({
-  defaultLayout: 'index',
-  layoutsDir: __dirname + '/views',
-}));
+app.set("view engine", "handlebars");
+app.engine(
+	"handlebars",
+	handlebars({
+		defaultLayout: "layout",
+		layoutsDir: __dirname + "/views",
+	})
+);
 
 app.use("/v1", v1Routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+	console.log(`Server is running on port: ${PORT}`);
 });
