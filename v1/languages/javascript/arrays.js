abc9b5f180a350d05acc29e86aa67b1e36dc1455
Renee de Kruijf <renee@webdesignpuntnl.com>
2020-06-12 15:39:46 +0200

    Create Javascript Features #15 Arrays

---
+++ v1/languages/javascript/arrays.js
@@ -0,0 +1,8 @@
+const Arrays = {
+  name: "Arrays",
+  description:
+    "Arrays are list-like objects whose prototype has methods to perform traversal and mutation operations. Neither the length of a JavaScript array nor the types of its elements are fixed. Since an array's length can change at any time, and data can be stored at non-contiguous locations in the array, JavaScript arrays are not guaranteed to be dense; this depends on how the programmer chooses to use them. You can declare them using square brackets. Items are separated by comma's and are zero-bases. So the first item starts at zero.",
+  example: "var list = ['a', 'b', 'c', 'd']; // console.log(list[0]) // 'a' ",
+};
+
+module.exports = Arrays;
