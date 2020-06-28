a982e3a0ddf764fa2878a47c068a4d7a4225e4d6
Edward Hong <edward.hong527@gmail.com>
2020-06-29 00:11:26 +1200

    body tag

---
+++ v1/languages/html/body.js
@@ -0,0 +1,16 @@
+const Body = {
+  name: "Body",
+  description:
+    "The <body> represents the content of an HTML document. There can be only one <body> element in a document.",
+  example: `
+      <html>
+        <head>
+          <title>Document title</title>
+        </head>
+        <body>
+          <p>This is a paragraph</p>
+        </body>
+      </html>`,
+};
+
+module.exports = Body;
