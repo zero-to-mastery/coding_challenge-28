25b91ebeda23b6ca7403864be0fc7240f0d8b68a
Edward Hong <edward.hong527@gmail.com>
2020-06-28 23:40:52 +1200

    br tag

---
+++ v1/languages/html/br.js
@@ -0,0 +1,17 @@
+const Br = {
+  name: "Br",
+  description:
+    "The <br> produces a line break in text (carriage-return). It is useful for writing a poem or an address, where the division of lines is significant.",
+  example: `
+    <p> O’er all the hilltops<br>
+      Is quiet now,<br>
+      In all the treetops<br>
+      Hearest thou<br>
+      Hardly a breath;<br>
+      The birds are asleep in the trees:<br>
+      Wait, soon like these<br>
+      Thou too shalt rest.
+    </p>`,
+};
+
+module.exports = Br;
