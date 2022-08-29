4ebf40c9bc6da8e7af506e3b8038e48bc8fda5ae
Moshfeq <moshfeqshahrani@gmail.com>
2022-08-28 18:00:29 -0700

    added number, string and null data types to javascript directory

---
+++ v1/languages/javascript/string.js
@@ -0,0 +1,9 @@
+const String = {
+    name: "string",
+    description: "String is a primitive type object which is used to hold and represent a sequence of characters. Strings can be created using single or double quotes or even the backtick character. They can even be created as objects using the String() constructor",
+    example: "const string20 = 'twenty', string20.length = 8, string20[1] = 't'",
+  };
+  
+  module.exports = String;
+  
+  
